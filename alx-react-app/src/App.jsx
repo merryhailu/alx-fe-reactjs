b435671f1a7ce588af90fc5bded1711f4780f5d3
Meron Hailu <hailumeron03@gmail.com>
2024-08-10 14:18:52 +0300

    task 2

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,12 +1,17 @@
 
-import './App.css'
-import WelcomeMessage from './components/WelcomeMessage'
+import './App.css';
+import WelcomeMessage from './components/WelcomeMessage';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
 
 function App() {
 
   return (
       <div>
-        <WelcomeMessage />
+        <Header/>
+        <MainContent/>
+        <Footer/>
       </div>
   );
 }
