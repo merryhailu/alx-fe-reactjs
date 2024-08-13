00d720f4107d21570896c00cfbf79806a070db28
Meron Hailu <hailumeron03@gmail.com>
2024-08-13 11:05:04 +0300

    correction

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,11 +1,13 @@
 
 import './App.css';
 import UserProfile from './components/UserProfile';
+import WelcomeMessage from './components/WelcomeMessage';
 
 function App() {
 
   return (
       <div>
+        <WelcomeMessage />
         <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       </div>
   );
