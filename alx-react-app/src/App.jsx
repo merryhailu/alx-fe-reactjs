430629f0122661a3c2cbc7851fc81748de58c551
Meron Hailu <hailumeron03@gmail.com>
2024-08-13 11:12:19 +0300

    correction

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,6 +2,9 @@
 import './App.css';
 import UserProfile from './components/UserProfile';
 import WelcomeMessage from './components/WelcomeMessage';
+import Header from './components/Header';
+import MainContet from './components/MainContent';
+import Footer from '/components/Footer';
 
 function App() {
 
