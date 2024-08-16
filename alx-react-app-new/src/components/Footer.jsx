04ce5e24c1f47ef156818f7686c0cb179b5151b4
Meron Hailu <hailumeron03@gmail.com>
2024-08-16 18:51:35 +0300

    week 10 project

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+
+function Footer(){
+    return(
+        <footer style={{ color: 'blue', lineHeight: 10, padding: 20 }}>
+            <p>© 2023 City Lovers</p>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,10 @@
+
+function MainContent(){
+    return(
+        <main style={{ color: 'blue', lineHeight: 10, padding: 20 }}>
+            <p>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    );
+}
+
+export default MainContent;
