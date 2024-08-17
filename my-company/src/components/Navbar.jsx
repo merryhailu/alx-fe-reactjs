64e65154b37bdb2d387780ce4c5368182b0821e8
Meron Hailu <hailumeron03@gmail.com>
2024-08-17 14:23:32 +0300

    task 3 v4

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 function Navbar() {
   return (
-    <nav style={{ padding: '10px', background: '#eee'}}>
+    <nav style={{ padding: '10px', backgroundColor: '#eee'}}>
         <ul style={{
             listStyleType: 'none',
             display: 'flex', justifyContent: 'center'}}>
