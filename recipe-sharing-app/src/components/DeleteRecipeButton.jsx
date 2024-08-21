a573832dafaa6597140200af4ac893062cb73edb
Meron Hailu <hailumeron03@gmail.com>
2024-08-21 12:27:24 +0300

    task 1 v5

--- recipe-sharing-app/src/components/DeleteRecipeButton.jsx
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -2,17 +2,18 @@
 
 import React from 'react';
 import  useRecipeStore  from './recipeStore';
+import { useNavigate } from 'react-router-dom';
 
 
 const DeleteRecipeButton = ({ recipeId }) => {
   const { deleteRecipe } = useRecipeStore();
- 
+  const navigate = useNavigate();
 
 
   const handleDelete = () => {
     if (window.confirm('Are you sure you want to delete this recipe?')) {
       deleteRecipe(recipeId);
-     
+      navigate('/recipes');
     }
 };
 
