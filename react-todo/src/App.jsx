9c86938ddec1bddf820672003f3561195a5ea261
Meron Hailu <hailumeron03@gmail.com>
2024-08-30 16:49:26 +0300

    task 3 v3

--- react-todo/src/App.jsx
+++ react-todo/src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
+import TodoList from './components/ToDoList';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Welcome to React</h1>
-      </header>
+    <div>
+     <TodoList />
+
     </div>
   );
 }
