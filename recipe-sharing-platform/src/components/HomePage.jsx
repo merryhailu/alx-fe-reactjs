5632f52c59ceafd2c39e10c41154d71921e602c2
Meron Hailu <hailumeron03@gmail.com>
2024-09-10 16:42:28 +0300

    task 1 v1

---
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { useState, useEffect } from 'react'
+import  RecipeCard  from './RecipeCard';
+
+
+function HomePage() {
+
+
+    const [recipes, setRecipes] = useState([]);
+
+    useEffect(() => {
+      const fetchData = async () => {
+        const response = await fetch('data.json');
+        const data = await response.json();
+        setRecipes(data);
+      };
+  
+      fetchData();
+    }, []);
+
+
+
+  return (
+    <div className='mx-auto py-8 bg-cyan-300'>
+        <h1 className='text-3xl text-center font-bold'>Welcome to My Recipe App</h1>
+        <div className='bg-red-300 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4' >
+            {recipes.map((recipe) => (
+            <RecipeCard key={recipe.id} recipe={recipe} />
+        ))} 
+        </div>
+
+
+    </div>
+  )
+}
+
+export default HomePage;
---
+++ recipe-sharing-platform/src/components/RecipeCard.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const RecipeCard = ({ recipe }) => {
+  return (
+    <div className='bg-slate-500'>
+        <h2>{recipe.title} </h2>
+        <img src="{recipe.image}" alt="{recipe.title}" />
+        <p>{recipe.summary} </p>
+        <button> View Recipe</button>
+    </div>
+  )
+}
+
+export default RecipeCard;
