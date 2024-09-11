5f3114fce00eab97bd07f0c9e3925e5c63c0d7c4
Meron Hailu <hailumeron03@gmail.com>
2024-09-11 11:38:27 +0300

    task 2 v1

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,6 +1,7 @@
-
-import './App.css'
-import HomePage from './components/HomePage'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './App.css';
+import HomePage from './components/HomePage';
+import RecipeDetail from './components/RecipeDetail';
 
 function App() {
   
@@ -8,8 +9,14 @@ function App() {
   return (
     <>
       <HomePage />
+<Router>
+  <Routes>
+     <Route path='/'  element={<HomePage />} />
+    <Route path='/recipe/:id' element={<RecipeDetail />} />
+  </Routes>
+</Router>
     </>
-  )
+  );
 }
 
 export default App;
