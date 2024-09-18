c81a36d81a61d14bb1ed599ff488f12c3a8f66f6
Meron Hailu <hailumeron03@gmail.com>
2024-09-18 20:53:10 +0300

    task 2 v6

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,7 +1,7 @@
 
 import axios from 'axios';
 
-const BASE_URL = 'https://api.github.com/search/users';
+const BASE_URL = 'https://api.github.com/search/users?q';
 
 const searchUsers = async (username, location, minRepos) => {
   const query = [
@@ -11,7 +11,7 @@ const searchUsers = async (username, location, minRepos) => {
   ].filter(Boolean).join(' ');
 
   try {
-    const response = await axios.get(`${BASE_URL}?q=${query}`);
+    const response = await axios.get(`${BASE_URL}=${query}`);
     return response.data.items;
   } catch (error) {
     console.error('Error fetching users:', error);
