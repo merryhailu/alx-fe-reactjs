b1b1898eb73c267323397d41c79364e7dc1045e0
Meron Hailu <hailumeron03@gmail.com>
2024-09-18 18:28:11 +0300

    task 0 v2

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom"
+
+function App() {
+ 
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
