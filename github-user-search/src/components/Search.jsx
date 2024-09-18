cf03b7bd79a48a6d9b210e276aaf906403f343c2
Meron Hailu <hailumeron03@gmail.com>
2024-09-18 19:00:08 +0300

    task 1 v1

---
+++ github-user-search/src/components/Search.jsx
@@ -0,0 +1,52 @@
+// src/components/Search.jsx
+import { useState } from 'react';
+import { fetchUserData } from '../services/githubService';
+
+const Search = () => {
+    const [username, setUsername] = useState('');
+    const [userData, setUserData] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    const handleSearch = async (e) => {
+        e.preventDefault();
+        setLoading(true);
+        setError(null);
+        try {
+            const data = await fetchUserData(username);
+            setUserData(data);
+        } catch (error) {
+            setError('Looks like we cant find the user');
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    return (
+        <div>
+            <form onSubmit={handleSearch}>
+                <input
+                    type="text"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    placeholder="Enter GitHub username"
+                />
+                <button type="submit">Search</button>
+            </form>
+            {loading && <p>Loading...</p>}
+            {error && <p>{error}</p>}
+            {userData && (
+                <div>
+                    <img src={userData.avatar_url} alt={userData.login} />
+                    <h2>{userData.name}</h2>
+                    <p>{userData.bio}</p>
+                    <a href={userData.html_url} target="_blank" rel="noopener noreferrer">
+                        View Profile
+                    </a>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default Search;
